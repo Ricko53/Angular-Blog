a356afaca53d7f40ade1da3a26b222cef97b1822
Jin Lang <2484882869@qq.com>
2015-04-18 22:20:16 +0800

    Menu Buttom

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@
   .config(function($mdThemingProvider, $mdIconProvider){
 
                   $mdIconProvider
-                      .defaultIconSet("./assets/svg/avatars.svg", 128)
-                      .icon("menu"       , "./assets/svg/menu.svg"        , 24)
-                      .icon("share"      , "./assets/svg/share.svg"       , 24)
-                      .icon("google_plus", "./assets/svg/google_plus.svg" , 512)
-                      .icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
-                      .icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
-                      .icon("phone"      , "./assets/svg/phone.svg"       , 512);
+                      .defaultIconSet("./img/icons/avatars.svg", 128)
+                      .icon("menu"       , "./img/icons/menu.svg"        , 24)
+                      .icon("share"      , "./img/icons/share.svg"       , 24)
+                      .icon("google_plus", "./img/icons/google_plus.svg" , 512)
+                      .icon("hangouts"   , "./img/icons/hangouts.svg"    , 512)
+                      .icon("twitter"    , "./img/icons/twitter.svg"     , 512)
+                      .icon("phone"      , "./img/icons/phone.svg"       , 512);
 
                       $mdThemingProvider.theme('default')
                           .primaryPalette('teal',{
